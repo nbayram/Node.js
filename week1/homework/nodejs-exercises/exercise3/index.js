591e86bd71af0da4bf3ab693a267232bbdb5281f
nbayram <nbayram4@gmail.com>
2019-11-20 12:29:31 +0100

    Create an HTTP web server

--- week1/homework/nodejs-exercises/exercise3/index.js
+++ week1/homework/nodejs-exercises/exercise3/index.js
@@ -4,19 +4,41 @@ var http = require('http');
 
 //create a server
 let server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  if (req.url === './script.js') {
-    // res.setHeader('Content-Type', 'text/javascript');
-    res.write("document.getElementById('content')  .appendChild(document.createTextNode('Welcome to Server-land!'));")
-  } else if (req.url === './style.css') {
-    res.write('<link rel="stylesheet" type="text/css" href="style.css" />')
-  } else {
-    // res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title>My First Web Server</title></head><body><h1>Hello, anyone there?</h1><div id="content"></div><script src="script.js"></script></body></html>'); //send a response back to the client
+
+  // http:\\localhost:3000\script.js
+  if (req.url === '/script.js') {
+    res.setHeader('Content-Type', 'text/javascript');
+    res.write(`document
+    .getElementById('content')
+    .appendChild(document.createTextNode('Welcome to Server-land!'));`)
   }
 
+  // http:\\localhost:3000\style.css
+  else if (req.url === '/style.css') {
+    res.setHeader('Content-Type', 'text/css');
+    res.write(`#content { color: blue }`);
+  }
+
+  else {
+
+    // Do not forget to set the content-type to text/html so that the browser knows how to deal with the response. Use the function response.setHeader(name, value)
+    res.setHeader('Content-Type', 'text/html');
+
+    // don't be afraid to copy-paste this directly in the javascript file using as a multiline string. You shouldn't use a separate html file for now.
+    // http:\\localhost:3000 and else
+    res.write(`<html>
+  <head>
+    <title>My First Web Server</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <h1>Hello, anyone there?</h1>
+    <div id="content"></div>
+    <script src="script.js"></script>
+  </body>
+</html>`); //send a response back to the client
+  }
   res.end(); //end the response
 });
 
 server.listen(3000); //the server listens on port 3000
-
