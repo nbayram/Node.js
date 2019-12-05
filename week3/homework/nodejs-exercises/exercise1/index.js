a0261cb9ccfd8ab7dee54892afdf03d58285d409
nbayram <nbayram4@gmail.com>
2019-12-05 15:04:04 +0100

    Add throw new Error on response

--- week3/homework/nodejs-exercises/exercise1/index.js
+++ week3/homework/nodejs-exercises/exercise1/index.js
@@ -12,16 +12,3 @@ fetch('http://api.icndb.com/jokes/random')
   })
   .then(cardOfJoke => console.log(cardOfJoke.value.joke)) // Print the joke to the console
   .catch(error => console.error(error.message));
-
-
-// const fetch = require('node-fetch');
-// fetch('http://api.icndb.com/jokes/random')
-//   .then(res => {
-//     if (res.ok) return res.json();
-//     else
-//       throw new Error(
-//         `Something went wrong: ${res.statusText} (${res.status})`,
-//       );
-//   })
-//   .then(({ value }) => console.log(value.joke))
-//   .catch(err => console.log(err));
