c6e24eeae3aab60ce6f547d4af00492f407e80c7
nbayram <nbayram4@gmail.com>
2019-12-05 15:01:43 +0100

    Catch the error if response doesn't get on exercise 1

--- week3/homework/nodejs-exercises/exercise1/index.js
+++ week3/homework/nodejs-exercises/exercise1/index.js
@@ -6,6 +6,22 @@ const fetch = require('node-fetch');
 
 // GET a random joke from the URL http://www.icndb.com/api/
 fetch('http://api.icndb.com/jokes/random')
-  .then(data => data.json())
+  .then(response => {
+    if (response.ok) return response.json();
+    else throw new Error(response.statusText);
+  })
   .then(cardOfJoke => console.log(cardOfJoke.value.joke)) // Print the joke to the console
-  .catch(console.error);
+  .catch(error => console.error(error.message));
+
+
+// const fetch = require('node-fetch');
+// fetch('http://api.icndb.com/jokes/random')
+//   .then(res => {
+//     if (res.ok) return res.json();
+//     else
+//       throw new Error(
+//         `Something went wrong: ${res.statusText} (${res.status})`,
+//       );
+//   })
+//   .then(({ value }) => console.log(value.joke))
+//   .catch(err => console.log(err));
